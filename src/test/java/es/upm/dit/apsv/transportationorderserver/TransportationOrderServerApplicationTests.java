ffa8bd24631602205d9bee206767f7e99e86d422
s.illescas <s.illescas@alumnos.upm.es>
2024-12-03 16:38:59 +0100

    git.yml

--- src/test/java/es/upm/dit/apsv/transportationorderserver/TransportationOrderServerApplicationTests.java
+++ src/test/java/es/upm/dit/apsv/transportationorderserver/TransportationOrderServerApplicationTests.java
@@ -1,11 +1,11 @@
 package es.upm.dit.apsv.transportationorderserver;
 
 import org.junit.jupiter.api.Test;
-//import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-//@SpringBootTest
-@SpringBootApplication
+@SpringBootTest
+//@SpringBootApplication
 class TransportationOrderServerApplicationTests {
 
 	@Test
